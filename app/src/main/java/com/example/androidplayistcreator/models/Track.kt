0b243cd866e912e861fb8647c89715f3797e799b
Steven Chouk <schouk@catmktg.com>
2025-01-24 11:49:27 +0100

    Adding duration on Track class

--- app/src/main/java/com/example/androidplayistcreator/models/Track.kt
+++ app/src/main/java/com/example/androidplayistcreator/models/Track.kt
@@ -1,8 +1,11 @@
 package com.example.androidplayistcreator.models
 
+import kotlin.time.Duration
+
 data class Track(
     val artist: String,
     val name: String,
     val step: Int,
-    val url: String
+    val url: String,
+    val duration: String
 )
