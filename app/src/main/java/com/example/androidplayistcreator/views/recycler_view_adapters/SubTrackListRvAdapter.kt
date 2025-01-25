b3bc1400486ac6f550b6d5283eed098cbe556ae0
Sheukn <104200110+Sheukn@users.noreply.github.com>
2025-01-25 16:26:05 +0100

    Adding Subtracks View Holder and adapters

---
+++ app/src/main/java/com/example/androidplayistcreator/views/recycler_view_adapters/SubTrackListRvAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.androidplayistcreator.views.recycler_view_adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidplayistcreator.models.Track
+import com.example.androidplayistcreator.views.viewholders.subTrackRvViewHolder
+
+class SubTrackListRvAdapter(val subTracks: List<Track>):
+    RecyclerView.Adapter<subTrackRvViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): subTrackRvViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.sub_track_view_holder, parent, false)
+        return subTrackRvViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: subTrackRvViewHolder, position: Int) {
+        val track = subTracks[position]
+        holder.bind(track)
+    }
+
+    override fun getItemCount(): Int {
+        return subTracks.size
+    }
+}
---
+++ app/src/main/java/com/example/androidplayistcreator/views/viewholders/subTrackRvViewHolder.kt
@@ -0,0 +1,27 @@
+package com.example.androidplayistcreator.views.viewholders
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidplayistcreator.R
+import com.example.androidplayistcreator.models.Track
+
+class subTrackRvViewHolder (ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+    val dotView: View = itemView.findViewById(R.id.dotView)
+    val trackTitle: TextView = itemView.findViewById(R.id.subTrackTextView)
+
+    fun bind(track: Track){
+        trackTitle.text = track.name
+        val random = randomMethod()
+        when(random){
+            1 -> dotView.setBackgroundResource(R.drawable.blue_circle_dot)
+            2 -> dotView.setBackgroundResource(R.drawable.green_circle_dot)
+            3 -> dotView.setBackgroundResource(R.drawable.red_circle_dot)
+        }
+    }
+
+    fun randomMethod() : Int{
+        // return 1 to 3
+        return (1..3).random()
+    }
+}
