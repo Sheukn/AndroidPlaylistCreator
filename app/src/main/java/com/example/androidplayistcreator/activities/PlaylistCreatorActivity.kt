a72dce02375fd2765930d07e5bd85f5600bfae2b
Steven Chouk <schouk@catmktg.com>
2025-02-17 12:11:03 +0100

    Adding player view

---
+++ app/src/main/java/com/example/androidplayistcreator/activities/PlaylistCreatorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidplayistcreator.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidplayistcreator.R
+
+class PlaylistCreatorActivity  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_playlist)
+    }
+}
