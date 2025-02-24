3d3c1906a552c662c33784790de4b229aa82751a
Sheukn <104200110+Sheukn@users.noreply.github.com>
2025-02-24 21:33:29 +0100

    quickfix layouts

--- app/src/main/java/com/example/androidplayistcreator/activities/PlaylistActivity.kt
+++ app/src/main/java/com/example/androidplayistcreator/activities/PlaylistActivity.kt
@@ -3,8 +3,8 @@ package com.example.androidplayistcreator.activities
 import PlaylistRepository
 import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,14 +12,14 @@ import com.example.androidplayistcreator.R
 import com.example.androidplayistcreator.views.recycler_view_adapters.PlaylistsRvAdapter
 
 class PlaylistActivity : AppCompatActivity() {
-    private lateinit var playlistNameEditText: EditText
+    private lateinit var userTextView: TextView
     private lateinit var createPlaylistButton: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playlist)
 
-        playlistNameEditText = findViewById(R.id.playlistNameEditText)
+        userTextView = findViewById(R.id.userTextView)
         createPlaylistButton = findViewById(R.id.createPlaylistButton)
 
         createPlaylistButton.setOnClickListener {
